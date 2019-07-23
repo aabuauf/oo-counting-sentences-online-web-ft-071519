31f9688b1c756be95ff94027c5628eab72e52fa0
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-23 11:46:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,11 +7,11 @@ class String
   end
 
   def question?
-
+    return true if self.end_with?("?")
   end
 
   def exclamation?
-
+    return true if self.end_with?("!")
   end
 
   def count_sentences
